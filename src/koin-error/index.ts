921803336604ebab072662bd1dfbc44aeeedb9c2
최원빈 <wonbeen@tossbank.com>
2024-04-08 00:38:49 +0900

    미사용 패키지 제거, action단에서 커밋 후 푸시하게 수정

--- src/koin-error/index.ts
+++ src/koin-error/index.ts
@@ -1,3 +1,4 @@
+/** 코인 서버 에러 인터페이스입니다. message, code를 담고있습니다. */
 export interface KoinError {
   type: "KOIN_ERROR";
   status: number;
@@ -5,6 +6,7 @@ export interface KoinError {
   message: string;
 }
 
+/** 해당 에러가 코인 서버에서 발생한 에러인지 판별합니다. */
 export function isKoinError(error: unknown): error is KoinError {
   try {
     return (error as KoinError).type === "KOIN_ERROR";
