26a4b4beed504b69ac026cce8347188fef643e07
Daeeui Kim <kimeodml@gmail.com>
2024-04-07 17:58:01 +0900

    chore: 패키지 수정
    Co-authored-by: 김혜준 <hyejun0228@users.noreply.github.com>
    Co-authored-by: Minjae Kim <smallkdb@gmail.com>

--- src/koin-error/index.ts
+++ src/koin-error/index.ts
@@ -1,9 +1,8 @@
-function add(a:number, b:number): number {
+export function add(a:number, b:number): number {
   return a + b;
 }
 
-function divide(a:number,b:number): number {
-  return a/b;
+export function divide(a:number,b:number): number {
+  return a / b;
 }
 
-export { add, divide }; 
